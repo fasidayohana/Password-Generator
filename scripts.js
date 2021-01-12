6c35496c9dcc820315e29b4fea04e6412244bb83
Yohana Fasida <fasidayohana@gmail.com>
2021-01-12 00:02:24 -0500

    Updating all pages
    Please enter the commit message for your changes. Lines starting

--- scripts.js
+++ scripts.js
@@ -1,13 +1,36 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+// Arrays for password
+var lowercaseAlphabet = ["a", "b", "c", "d", "e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z",];
+var uppercaseAlphabet = ["A", "B", "C", "D", "E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z",];
+var numbers = ["0", "1", "2", "3", "4","5","6","7","8","9",];
+var specialCharacters = ["!", "@", "#", "$", "%","^","&","*","(",")","_","+","=","-","<",">","?","[","]","{","}","~",];
 
-  passwordText.value = password;
+// Variables for password
+var passwordLength;
+var chooseLowercaseAlphabet;
+var chooseUppercaseAlphabet;
+var chooseNumbers;
+var chooseSpecialCharacters;
 
+
+ //Beginning function for password - determining length
+function determinePasswordLength (){
+  var passwordLength = prompt("How many characters do you want your password to have");
+
+if (passwordLength < 8) {
+  alert("Password must be at least 8 characters minimum");
+  return;
+}
+else if (passwordLength > 128) {
+  alert ("Password must be less then 128 characters maximum");
+  return;
+}
+};
+
+function determineLowercaseAlphabet (){
+  LowercaseAlphabet = prompt("Would you like uppercase letters in yor password")
 }
 
 // Add event listener to generate button
