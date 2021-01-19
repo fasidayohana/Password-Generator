ab41dca21ce75a655603c82a85d3f57a0b657f3e
Yohana Fasida <fasidayohana@gmail.com>
2021-01-18 19:08:44 -0500

    edits to all the pages

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # Password-Generator
+
+Description:
+This application is a random passwrd generator, its randomly generates passwords through selected answers and criteria. Once prompts are answered it creates a user geerated response. 
+*STILL UNDER CONSTRUCTION*
+
+Installtion:
+Visit 
+
+to either fork or clome the repository located on GitHub
+
+Instructions:
+Application determines a random password based on the following statistics:
+
+1. Password Length
+2. Character Types:
+-Special Characters
+-Numeric Characters
+-Lowercase Letters
+-Uppercase Letters
--- scripts.js
+++ scripts.js
@@ -1,37 +1,44 @@
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
-
-// Arrays for password
+  // Arrays for password
 var lowercaseAlphabet = ["a", "b", "c", "d", "e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z",];
 var uppercaseAlphabet = ["A", "B", "C", "D", "E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z",];
 var numbers = ["0", "1", "2", "3", "4","5","6","7","8","9",];
 var specialCharacters = ["!", "@", "#", "$", "%","^","&","*","(",")","_","+","=","-","<",">","?","[","]","{","}","~",];
 
 // Variables for password
-var passwordLength;
+var passwordLength; 
 var chooseLowercaseAlphabet;
 var chooseUppercaseAlphabet;
 var chooseNumbers;
 var chooseSpecialCharacters;
 
+//Function determining password length
+var generatePasswordButton = document.getElementById("generate");
 
- //Beginning function for password - determining length
-function determinePasswordLength (){
-  var passwordLength = prompt("How many characters do you want your password to have");
+function generatePassword() {
 
-if (passwordLength < 8) {
-  alert("Password must be at least 8 characters minimum");
-  return;
-}
-else if (passwordLength > 128) {
-  alert ("Password must be less then 128 characters maximum");
-  return;
-}
+  var passwordLength
+  prompt("How many characters would you like your password to be?");
+  alert("Passowrd must be less the 8 characters but not exceed 128 characters");
+  while(passwordLength <=7 || passwordLength >=129);
 };
 
-function determineLowercaseAlphabet (){
-  LowercaseAlphabet = prompt("Would you like uppercase letters in yor password")
-}
+//Function determining password variables
+function chooseVariables() {
+var chooseLowercaseAlphabet = 
+confirm("Would you like lower case letters in your password?");
+confirm("Would you like upper case letters in your password?");
+confirm("Would you like numbers in your password?");
+confirm("Would you like special characters in your password?");
+Array.push.apply(specialCharacters);
+};
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+//Generate Function Password
+var characters = "";
+characters += (chooseLowercaseAlphabet? lowercaseAlphabet: "");
+characters += (chooseUppercaseAlphabet? uppercaseAlphabet: "");
+characters += (chooseNumbers? numbers: "");
+characters += (chooseSpecialCharacters? specialCharacters: "");
+
+function password(passwordLength, characters) {
+document.getElementById("password").innerHTML = pwd;
+};
